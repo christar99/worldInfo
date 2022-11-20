6bd8d080c7a2f7fda3e501126d04e017a476764d
이동아 <christar99@naver.com>
2022-11-20 21:59:37 +0900

    browserRouter add basename

--- src/common/Router.tsx
+++ src/common/Router.tsx
@@ -1,17 +1,16 @@
-import { Routes, Route } from "react-router-dom";
-import PageNotFound from "pages/PageNotFound";
-import WorldNews from "pages/WorldNews";
-import Statistics from "pages/Statistics";
-import Nations from "pages/Nation";
-
+import { Routes, Route } from 'react-router-dom';
+import PageNotFound from 'pages/PageNotFound';
+import WorldNews from 'pages/WorldNews';
+import Statistics from 'pages/Statistics';
+import Nations from 'pages/Nation';
 
 const Router = () => (
-    <Routes>
-        <Route path="/" element={<WorldNews />} />
-        <Route path="/statistics" element={<Statistics />} />
-        <Route path="/nation/:id" element={<Nations />} />
-        <Route path="*" element={<PageNotFound />} />
-    </Routes>
-)
+	<Routes>
+		<Route path="/" element={<WorldNews />} />
+		<Route path="/statistics" element={<Statistics />} />
+		<Route path="/nation/:id" element={<Nations />} />
+		<Route path="*" element={<PageNotFound />} />
+	</Routes>
+);
 
 export default Router;
