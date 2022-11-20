51d6d5174931fab2a4cb0b549a991df1291093b6
이동아 <christar99@naver.com>
2022-11-20 21:59:52 +0900

    browserRouter add basename

--- src/index.tsx
+++ src/index.tsx
@@ -1,29 +1,24 @@
-import ReactDOM from "react-dom/client";
-import { Provider } from "react-redux";
-import App from "./App";
-import reportWebVitals from "./reportWebVitals";
-import store from "./store";
+import ReactDOM from 'react-dom/client';
+import { Provider } from 'react-redux';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+import store from './store';
 import { BrowserRouter as Router } from 'react-router-dom';
-import dayjs from "dayjs";
-import isLeapYear from "dayjs/plugin/isLeapYear";
-import "dayjs/locale/lo";
+import dayjs from 'dayjs';
+import isLeapYear from 'dayjs/plugin/isLeapYear';
+import 'dayjs/locale/lo';
 
-const root = ReactDOM.createRoot(
-    document.getElementById("root") as HTMLElement
-);
+const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 
 dayjs.extend(isLeapYear);
 dayjs.locale('ko');
 
 root.render(
-    <Provider store={store}>
-        <Router>
-            <App />
-        </Router>
-    </Provider>
+	<Provider store={store}>
+		<Router basename="/worldinfo">
+			<App />
+		</Router>
+	</Provider>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
